ca330b22a461177be48f26f9a75a6828f5bc45e7
Serhii Zadorozhnii <wiseacreserj@gmail.com>
2024-03-08 00:40:45 +0200

    project dont

--- script.js
+++ script.js
@@ -26,6 +26,10 @@ const choices = {
     spock: { name: "Spock", defeats: ["scissors", "rock"] },
 };
 
+let playerScoreNumber = 0;
+let computerScoreNumber = 0;
+let computerChoice = "";
+
 //Passing player selection value and styling icon
 
 //Reset all "selected" icons
@@ -34,8 +38,87 @@ const resetSelected = () => {
     allGameIcons.forEach((icon) => icon.classList.remove("selected"));
 };
 
-const select = (playerChoice) => {
+const resetAll = () => {
+    playerScoreNumber = 0;
+    computerScoreNumber = 0;
+    playerScoreEl.textContent = playerScoreNumber;
+    computerScoreEl.textContent = computerScoreNumber;
+    playerChoiceEl.textContent = "";
+    computerChoiceEl.textContent = "";
+    resultText.textContent = "";
     resetSelected();
+};
+
+const computerRandomChoice = () => {
+    const computerChoiceNumber = Math.random();
+    if (computerChoiceNumber < 0.2) {
+        computerChoice = "rock";
+    } else if (computerChoiceNumber <= 0.4) {
+        computerChoice = "paper";
+    } else if (computerChoiceNumber <= 0.6) {
+        computerChoice = "scissors";
+    } else if (computerChoiceNumber <= 0.8) {
+        computerChoice = "lizard";
+    } else {
+        computerChoice = "spock";
+    }
+};
+
+//Add "selected" to computer choice
+
+const displayComputerChoice = (p) => {
+    switch (computerChoice) {
+        case "rock":
+            computerRock.classList.add("selected");
+            computerChoiceEl.textContent = " --- Rock";
+            break;
+        case "paper":
+            computerPaper.classList.add("selected");
+            computerChoiceEl.textContent = " --- Paper";
+            break;
+        case "scissors":
+            computerScissors.classList.add("selected");
+            computerChoiceEl.textContent = " --- Scissors ";
+            break;
+        case "lizard":
+            computerLizard.classList.add("selected");
+            computerChoiceEl.textContent = " --- Lizard";
+            break;
+        case "spock":
+            computerSpock.classList.add("selected");
+            computerChoiceEl.textContent = " --- Spock";
+            break;
+        default:
+            break;
+    }
+};
+
+const updateScore = (playerChoice) => {
+    if (playerChoice === computerChoice) {
+        resultText.textContent = "It's a tie.";
+    } else {
+        const choice = choices[playerChoice];
+        if (choice.defeats.indexOf(computerChoice) > -1) {
+            resultText.textContent = "You Won!";
+            playerScoreNumber++;
+            playerScoreEl.textContent = playerScoreNumber;
+        } else {
+            resultText.textContent = "You Lost!";
+            computerScoreNumber++;
+            computerScoreEl.textContent = computerScoreNumber;
+        }
+    }
+};
+
+const checkResult = (playerChoice) => {
+    resetSelected();
+    computerRandomChoice();
+    displayComputerChoice();
+    updateScore(playerChoice);
+};
+
+const select = (playerChoice) => {
+    checkResult(playerChoice);
     switch (playerChoice) {
         case "rock":
             playerRock.classList.add("selected");
