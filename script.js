f065999c929e73843c94edb40167c8e0075d33c0
Serhii Zadorozhnii <wiseacreserj@gmail.com>
2024-03-07 23:44:12 +0200

    add JS code for selection

--- script.js
+++ script.js
@@ -1,10 +1,63 @@
+const playerScoreEl = document.querySelector("#playerScore");
+const playerChoiceEl = document.querySelector("#playerChoice");
+const computerScoreEl = document.querySelector("#computerScore");
+const computerChoiceEl = document.querySelector("#computerChoice");
+const resultText = document.querySelector("#resultText");
 
+const playerRock = document.querySelector("#playerRock");
+const playerPaper = document.querySelector("#playerPaper");
+const playerScissors = document.querySelector("#playerScissors");
+const playerLizard = document.querySelector("#playerLizard");
+const playerSpock = document.querySelector("#playerSpock");
+
+const computerRock = document.querySelector("#computerRock");
+const computerPaper = document.querySelector("#computerPaper");
+const computerScissors = document.querySelector("#computerScissors");
+const computerLizard = document.querySelector("#computerLizard");
+const computerSpock = document.querySelector("#computerSpock");
+
+const allGameIcons = document.querySelectorAll(".far");
 
 const choices = {
-  rock: { name: 'Rock', defeats: ['scissors', 'lizard'] },
-  paper: { name: 'Paper', defeats: ['rock', 'spock'] },
-  scissors: { name: 'Scissors', defeats: ['paper', 'lizard'] },
-  lizard: { name: 'Lizard', defeats: ['paper', 'spock'] },
-  spock: { name: 'Spock', defeats: ['scissors', 'rock'] },
+    rock: { name: "Rock", defeats: ["scissors", "lizard"] },
+    paper: { name: "Paper", defeats: ["rock", "spock"] },
+    scissors: { name: "Scissors", defeats: ["paper", "lizard"] },
+    lizard: { name: "Lizard", defeats: ["paper", "spock"] },
+    spock: { name: "Spock", defeats: ["scissors", "rock"] },
 };
 
+//Passing player selection value and styling icon
+
+//Reset all "selected" icons
+
+const resetSelected = () => {
+    allGameIcons.forEach((icon) => icon.classList.remove("selected"));
+};
+
+const select = (playerChoice) => {
+    resetSelected();
+    switch (playerChoice) {
+        case "rock":
+            playerRock.classList.add("selected");
+            playerChoiceEl.textContent = " --- Rock";
+            break;
+        case "paper":
+            playerPaper.classList.add("selected");
+            playerChoiceEl.textContent = " --- Paper";
+            break;
+        case "scissors":
+            playerScissors.classList.add("selected");
+            playerChoiceEl.textContent = " --- Scissors ";
+            break;
+        case "lizard":
+            playerLizard.classList.add("selected");
+            playerChoiceEl.textContent = " --- Lizard";
+            break;
+        case "spock":
+            playerSpock.classList.add("selected");
+            playerChoiceEl.textContent = " --- Spock";
+            break;
+        default:
+            break;
+    }
+};
